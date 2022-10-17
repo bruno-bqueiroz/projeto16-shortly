8938a35aad77fd544814880766bd7a17516ab079
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-10-17 10:21:26 -0400

    config heroku

--- database/database.js
+++ database/database.js
@@ -1,10 +1,14 @@
 import pg from 'pg';
+
 const { Pool } = pg;
-import dotenv from 'dotenv';
-dotenv.config();
 
-const connection = new Pool({
+const databaseConfig = {
     connectionString: process.env.DATABASE_URL,
-  });
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
 
-  export { connection };
+const connection = new Pool(databaseConfig);
+
+export {connection};
